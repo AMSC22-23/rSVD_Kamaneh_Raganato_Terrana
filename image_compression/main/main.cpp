43a4916723bf908aef121db01a87810b5f2d4224
yektaKamane <ykamaneh@gmail.com>
2024-01-17 09:57:27 +0000

    working on loading the image

--- image_compression/main/main.cpp
+++ image_compression/main/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <mpi.h>
+#include <Eigen/Dense>
+#include "rSVD.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char** argv) {
+
+    MPI_Init(&argc, &argv);
+    int num_procs, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    if (rank == 0) std::cout << "main running" << std::endl;
+
+    // int m = A.rows();
+    // int n = A.cols();
+    // int k = 0; // numerical rank (we need an algorithm to find it) or target rank
+    // int p = 16; // oversampling parameter, usually it is set to 5 or 10
+    // int l = k + p;
+    // Mat A_copy = A;
+    // Mat U = Mat::Zero(m, l);
+    // Vec S = Vec::Zero(l);
+    // Mat V = Mat::Zero(l, n);
+    // rSVD(A, U, S, V, l);
+
+
+    MPI_Finalize();
     return 0;
 }
