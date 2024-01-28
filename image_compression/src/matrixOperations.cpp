b9fa1ae046c3077cc5808d23fc95f24dd12b25f9
yektaKamane <ykamaneh@gmail.com>
2024-01-28 10:04:54 +0000

    minor change

--- image_compression/src/matrixOperations.cpp
+++ image_compression/src/matrixOperations.cpp
@@ -56,7 +56,6 @@ Mat manualMatrixMultiply(const Mat &A, const Mat &B) {
         std::vector<int> recvcounts(num_procs);
         std::vector<int> displacements(num_procs);
 
-        // #pragma omp simd
         for (int i = 0; i < num_procs; ++i) {
             recvcounts[i] = (i < rows_remainder) ? rows_per_proc + 1 : rows_per_proc;
             displacements[i] = i * rows_per_proc + std::min(i, rows_remainder);
@@ -71,38 +70,5 @@ Mat manualMatrixMultiply(const Mat &A, const Mat &B) {
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
 
-    if (rank == 0) {
-    // //     // std::cout << "Matrix A:\n" << A << "\n\n";
-    // //     // std::cout << "Matrix B:\n" << B << "\n\n";
-    // //     // std::cout << "Matrix C (Result):\n" << C << "\n\n";
-
-    // //     // std::cout << "Actual c:\n" << A * B << std::endl;
-        std::cout << "norm of diff: " << (C - A*B).norm() << "\n";
-        std::cout << "Execution Time: " << duration.count() << " microseconds\n";
-    }
-
     return C;
 }
-
-// int main(int argc, char** argv) {
-//     // Initialize MPI
-//     MPI_Init(&argc, &argv);
-
-//     Eigen::MatrixXd A(300, 300);
-//     Eigen::MatrixXd B(300, 300);
-//     // Eigen::MatrixXd C(3, 3); 
-//     A.setRandom();
-//     B.setRandom();
-//     // A << 1, 2, 3,
-//     //      4, 5, 6,
-//     //      10, 8, 9;
-
-//     // B << 1, 1, 1,
-//     //      1, 5, 1,
-//     //      1, 1, 1;
-
-//     Mat C = manualMatrixMultiply(A, B);
-
-//     MPI_Finalize();
-//     return 0;
-// }
