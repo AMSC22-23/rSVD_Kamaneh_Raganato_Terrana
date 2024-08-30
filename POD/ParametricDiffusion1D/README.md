34077271633fe43e8c77f32376c3cdd70a0db4a3
elisa <elisa.terrana@mail.polimi.it>
2024-08-30 17:43:33 +0000

    Choice for initial condition added, plot of solutions and errors

---
+++ POD/ParametricDiffusion1D/README.md
@@ -0,0 +1,6 @@
+
+
+```
+./Diff1D ../input/test_pod_more_prm.txt ../input/test_advdiff.prm 
+../script/plot_solution.py ../output/full.mtx ../output/reconstruction.mtx ../output/errors.mtx ../input/test_pod_more_prm.txt
+```
--- POD/ParametricDiffusion1D/src/Diff1D_convergence.cpp
+++ POD/ParametricDiffusion1D/src/Diff1D_convergence.cpp
@@ -106,6 +106,11 @@ main(int argc, char * argv[])
       prm_diffusion_coefficient[i] = (mu_min+i*(mu_max-mu_min)/(n-1));
     pcout << "  Check prm_diffusion_coefficient = " << prm_diffusion_coefficient[i] << std::endl;
   }
+
+  const std::vector<double> deltat_vector = {
+  0.25, 0.125, 0.0625, 0.03125, 0.015625};
+  std::vector<std::vector<double>> errors_L2; // doppio per 
+  std::vector<double> errors_H1;
     
   auto start_snapshot = high_resolution_clock::now();
   for (unsigned int i=0; i<n; i++)
@@ -297,6 +302,74 @@ main(int argc, char * argv[])
   // Export the vector containing the relative errors for all rom_sizes and all parameters. POI SARÀ SOLO NEW
   std::string matrixFileOut3("../output/errors.mtx");
   Eigen::saveMarket(errors, matrixFileOut3); 
+
+
+    const std::vector<double> deltat_vector = {
+    0.25, 0.125, 0.0625, 0.03125, 0.015625};
+  std::vector<double> errors_L2;
+  std::vector<double> errors_H1;
+
+  for (const auto &deltat : deltat_vector)
+    {
+      Heat problem("../mesh/mesh-cube-20.msh", degree, T, deltat, theta);
+
+      problem.setup();
+      problem.solve();
+
+      errors_L2.push_back(problem.compute_error(VectorTools::L2_norm));
+      errors_H1.push_back(problem.compute_error(VectorTools::H1_norm));
+    }
+
+  // Print the errors and estimate the convergence order.
+  if (mpi_rank == 0)
+    {
+      std::cout << "==============================================="
+                << std::endl;
+
+      std::ofstream convergence_file("convergence.csv");
+      convergence_file << "dt,eL2,eH1" << std::endl;
+
+      for (unsigned int i = 0; i < deltat_vector.size(); ++i)
+        {
+          convergence_file << deltat_vector[i] << "," << errors_L2[i] << ","
+                           << errors_H1[i] << std::endl;
+
+          std::cout << std::scientific << "dt = " << std::setw(4)
+                    << std::setprecision(2) << deltat_vector[i];
+
+          std::cout << std::scientific << " | eL2 = " << errors_L2[i];
+
+          // Estimate the convergence order.
+          if (i > 0)
+            {
+              const double p =
+                std::log(errors_L2[i] / errors_L2[i - 1]) /
+                std::log(deltat_vector[i] / deltat_vector[i - 1]);
+
+              std::cout << " (" << std::fixed << std::setprecision(2)
+                        << std::setw(4) << p << ")";
+            }
+          else
+            std::cout << " (  - )";
+
+          std::cout << std::scientific << " | eH1 = " << errors_H1[i];
+
+          // Estimate the convergence order.
+          if (i > 0)
+            {
+              const double p =
+                std::log(errors_H1[i] / errors_H1[i - 1]) /
+                std::log(deltat_vector[i] / deltat_vector[i - 1]);
+
+              std::cout << " (" << std::fixed << std::setprecision(2)
+                        << std::setw(4) << p << ")";
+            }
+          else
+            std::cout << " (  - )";
+
+          std::cout << "\n";
+        }
+    }
 
   return 0;
 }
